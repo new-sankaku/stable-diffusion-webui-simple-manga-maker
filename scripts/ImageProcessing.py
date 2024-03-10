1a7a91f85280adb55f20e93ed4cd7367bcd06ff0
new-sankaku <sakurasakusankaku@gmail.com>
2024-03-10 22:40:17 +0900

    適用画像を中心をベースになるように変更

--- scripts/ImageProcessing.py
+++ scripts/ImageProcessing.py
@@ -26,8 +26,8 @@ def createInfomation(jsonFile, next_working_number, now_working_max_number):
     result = result + f"Next Accept Ratio : X={simplified_width}, Y={simplified_height}\n"
     return result
 
+# 簡素化された幅と高さを元に、それらが一定のサイズを超えるまで倍増させる
 def adjust_dimensions(simplified_width, simplified_height):
-
     start_simplified_width = simplified_width
     start_simplified_height = simplified_height
 
@@ -39,6 +39,7 @@ def adjust_dimensions(simplified_width, simplified_height):
     
     return simplified_width, simplified_height
 
+# 幅と高さからアスペクト比を求め、それを最も簡単な形で返す
 def get_aspect_ratio(width, height):
     width = width // 8
     height = height // 8
@@ -50,31 +51,36 @@ def get_aspect_ratio(width, height):
 
     return simplified_width, simplified_height
 
-def getBlackLineFileName( image_path ) :
-    print(f"getImagePlusNumberFileName image_path:{image_path}")
+# 黒線が追加された画像のファイル名を生成する
+def getBlackLineFileName(image_path):
+    print(f"getBlackLineFileName image_path:{image_path}")
     base_name = os.path.splitext(os.path.basename(image_path))[0]
     image_filename = f"{base_name}_black_line.png"
     return ImageManager.manga_panels_image_info_path + "\\" + image_filename
 
-
-def getImagePlusNumberFileName( image_path ) :
+# 通し番号が追加された画像のファイル名を生成する
+def getImagePlusNumberFileName(image_path):
     print(f"getImagePlusNumberFileName image_path:{image_path}")
     base_name = os.path.splitext(os.path.basename(image_path))[0]
     image_filename = f"{base_name}_plus_number.png"
     return ImageManager.manga_panels_image_info_path + "\\" + image_filename
 
-def getImagePlusJsonFileName( image_path ) :
+# パネル情報が含まれるJSONファイルの名前を生成する
+def getImagePlusJsonFileName(image_path):
     print(f"getImagePlusJsonFileName image_path:{image_path}")
     base_name = os.path.splitext(os.path.basename(image_path))[0]
     json_filename = f"{base_name}_plus_panel.json"
     return ImageManager.manga_panels_image_info_path + "\\" + json_filename
 
-def getImageFileNamePlusyyyyMMddhhmmss( image_path ) :
+# 現在の日時を含んだ画像ファイル名を生成する
+def getImageFileNamePlusyyyyMMddhhmmss(image_path):
     current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    print(f"getImageFileNamePlusyyyyMMddhhmmss image_path:{image_path}")
     base_name = os.path.splitext(os.path.basename(image_path))[0]
     json_filename = f"{base_name}_Insert_{current_time}.png"
     return ImageManager.manga_panels_image_info_path + "\\" + json_filename
 
+# JSONファイルから指定された番号のパネルの幅と高さを計算します。
 def get_dimensions(json_file_path, number):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
@@ -88,6 +94,7 @@ def get_dimensions(json_file_path, number):
     height = max(y_coords) - min(y_coords)
     return width, height
 
+# JSONファイルから最大のパネル番号を取得します。
 def get_max_panel_number(json_file_path):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
@@ -95,14 +102,13 @@ def get_max_panel_number(json_file_path):
     max_panel_number = max(panel_numbers)
     return max_panel_number
 
+# JSONファイルから指定されたパネルの中心座標を計算します。
 def get_panel_center(json_file_path, panel_number):
     with open(json_file_path, 'r') as file:
         data = json.load(file)
     
-    # 指定された番号のパネルの座標を取得
     panel_coords = data[str(panel_number)]
     
-    # 中心座標を計算
     sum_x = sum_y = 0
     for coord in panel_coords:
         sum_x += coord['x']
@@ -112,6 +118,7 @@ def get_panel_center(json_file_path, panel_number):
     
     return center_x, center_y
 
+# 輪郭を左上の角でソートします。画像内のオブジェクトを左上から右下へ順に並べます。
 def sort_contours_by_top_left_corner(contours):
     bounding_boxes = [cv2.boundingRect(c) for c in contours]
     contours_with_bb = zip(contours, bounding_boxes)
@@ -119,6 +126,7 @@ def sort_contours_by_top_left_corner(contours):
     sorted_contours = [c[0] for c in sorted_contours_with_bb]
     return sorted_contours
 
+# 輪郭を水平方向にソートします。主に左から右への順序でオブジェクトを並べ替える際に使用します。
 def sort_contours_horizontally(contours):
     bounding_boxes = [cv2.boundingRect(c) for c in contours]
     contours_with_bb = zip(contours, bounding_boxes)
@@ -135,10 +143,12 @@ def find_comic_panel_coords_and_save(image_path):
     _, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
     thresh = cv2.bitwise_not(thresh)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
     contours = sort_contours_by_top_left_corner(contours)
 
     panels_info = {}
     panel_number = 1
+    total_pixels = temp_img.shape[0] * temp_img.shape[1]
     for cnt in contours:
         if cv2.contourArea(cnt) < 1000:
             continue
@@ -151,9 +161,10 @@ def find_comic_panel_coords_and_save(image_path):
             panel_coords = [{'x': int(point[0][0]), 'y': int(point[0][1])} for point in approx]
             panels_info[panel_number] = panel_coords
 
-            area = cv2.contourArea(cnt)
-            font_scale = min(6.0, 1.0 + area / (temp_img.shape[0] * temp_img.shape[1]) * 100)
-            cv2.putText(temp_img, str(panel_number), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 0), 12, cv2.LINE_AA)
+
+            base_font_scale = 3
+            font_scale = max(base_font_scale, (total_pixels ** 0.5) / 1000)
+            cv2.putText(temp_img, str(panel_number), (cx, cy), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 0, 0), 3, cv2.LINE_AA)
             panel_number += 1
 
     json_filename = getImagePlusJsonFileName(image_path)
@@ -237,9 +248,26 @@ def warp_insert_image_improved(comic_img, insert_img, panel_coords):
 
     # 挿入する画像をリサイズ（パネルを完全に埋めるように）
     insert_img_resized = resize_fill_panel(insert_img, w, h)
-    
-    # 挿入画像をパネルの大きさにクリップ（はみ出さないように）
-    insert_img_clipped = insert_img_resized[:h, :w]
+
+    # 挿入画像を中心から切り取る処理を追加
+    if insert_img_resized.shape[1] > w:
+        # 新しい画像がパネルの幅よりも広い場合、中央から切り取る
+        center_x = insert_img_resized.shape[1] // 2
+        start_x = max(center_x - w // 2, 0)
+        end_x = start_x + w
+        insert_img_clipped = insert_img_resized[:, start_x:end_x]
+    else:
+        insert_img_clipped = insert_img_resized
+
+    if insert_img_resized.shape[0] > h:
+        # 新しい画像がパネルの高さよりも高い場合、中央から切り取る
+        center_y = insert_img_resized.shape[0] // 2
+        start_y = max(center_y - h // 2, 0)
+        end_y = start_y + h
+        insert_img_clipped = insert_img_clipped[start_y:end_y, :]
+
+    # # 挿入画像をパネルの大きさにクリップ（はみ出さないように）
+    # insert_img_clipped = insert_img_resized[:h, :w]
 
     # 挿入画像をパネル位置に合わせて配置
     insert_region = comic_img[y:y+h, x:x+w]
@@ -277,50 +305,3 @@ def remove_black_lines_in_insert_area(black_lines_mask, x, y, w, h):
     """
     black_lines_mask[y:y+h, x:x+w] = False
     return black_lines_mask
-
-# panel_number = 1
-
-# 画像のパス
-# koma_img_path = 'koma_2.png'
-# insert_img_path = '10.png'
-# comic_img = cv2.imread(koma_img_path)
-# insert_img = cv2.imread(insert_img_path)
-# json_filename = getImagePlusJsonFileName(koma_img_path)
-# image_filename = getImagePlusNumberFileName(koma_img_path)
-
-# if os.path.exists(json_filename):
-#     print(f"exists:{json_filename}")
-# else:
-#     panels_info = find_comic_panel_coords_and_save(koma_img_path)
-
-# json_max = get_max_panel_number(json_filename)
-# # print(f"json_max:{json_max}")
-
-# center_x, center_y = get_panel_center(json_filename, panel_number)
-# seed_point = (center_x, center_y)
-# # print(f"center_x:{center_x}, center_y{center_y}")
-
-
-# black_line_file_path = koma_img_path.rsplit('.', 1)[0] + '_black_line.png'
-# if os.path.exists(black_line_file_path):
-#     print("black_line_file_path is exist")
-#     black_lines_mask = cv2.imread(black_line_file_path, cv2.IMREAD_GRAYSCALE)
-#     black_lines_mask = black_lines_mask == 255
-# else:
-#     print("black_line_file_path is not exist")
-#     black_lines_mask = extract_black_lines(comic_img)
-#     cv2.imwrite(black_line_file_path, black_lines_mask.astype(np.uint8) * 255)
-
-# # コミックパネルの座標を見つける
-# panel_coords = find_comic_panel_coords(comic_img, seed_point)
-# # Insert image.
-# if panel_coords is not None:
-#     combined_image = warp_insert_image_improved(comic_img, insert_img, panel_coords)
-# else:
-#     combined_image = comic_img
-
-# createImageFileName = getImageFileNamePlusyyyyMMddhhmmss(koma_img_path);
-
-# # 黒線の枠を出力画像に上書き
-# final_image_with_black_lines = apply_black_lines(combined_image, black_lines_mask)
-# cv2.imwrite(createImageFileName, final_image_with_black_lines)
