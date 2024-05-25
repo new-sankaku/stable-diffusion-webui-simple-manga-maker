e48dc8c04bfb8c4b7cbb30e9dde3a8812cc8b667
new-sankaku <sakurasakusankaku@gmail.com>
2024-05-25 14:12:57 +0900

    ADD:Crop mode

---
+++ SP-MangaEditer/js/Anime.js
@@ -0,0 +1,7 @@
+
+
+let anims = [...document.querySelectorAll("[anim]")];
+let click = (el, cb) => el.addEventListener("click", cb);
+let toggle = (el) => el.classList.toggle("toggled");
+let clickTog = (el) => click(el, () => toggle(el));
+anims.map(clickTog);
