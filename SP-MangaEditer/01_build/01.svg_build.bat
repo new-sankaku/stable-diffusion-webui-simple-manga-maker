0e86de2ef4e0d5bc817aa9fc85c0df00745588ff
new-sankaku <sakurasakusankaku@gmail.com>
2025-03-01 02:33:58 +0900

    ビルドで作られるJS名を変更、リファクタリングで残った無駄ファイルを変更

--- SP-MangaEditer/01_build/01.svg_build.bat
+++ SP-MangaEditer/01_build/01.svg_build.bat
@@ -1,4 +1,4 @@
-python SvgBuilder.py ../02_images_svg/MangaPanelsImage/Landscape ../js/svg/MangaPanelsImage_Landscape.js MangaPanelsImage_Landscape
-python SvgBuilder.py ../02_images_svg/MangaPanelsImage/Vertical ../js/svg/MangaPanelsImage_Vertical.js MangaPanelsImage_Vertical
-python SvgBuilder.py ../02_images_svg/SpeechBubble ../js/svg/SpeechBubble.js SpeechBubble
+python SvgBuilder.py ../02_images_svg/MangaPanelsImage/Landscape ../js/svg/manga-panels-image-landscape.js MangaPanelsImage_Landscape
+python SvgBuilder.py ../02_images_svg/MangaPanelsImage/Vertical ../js/svg/manga-panels-image-vertical.js MangaPanelsImage_Vertical
+python SvgBuilder.py ../02_images_svg/SpeechBubble ../js/svg/speechbubble.js SpeechBubble
 PAUSE echo end.
